0933adc4f86d7b8296b75b9580fc487d06d72c2c
IyasuH <eyasuhailegbr@gmail.com>
2024-04-26 12:48:45 +0300

    model updated

--- bookStoreApp/models.py
+++ bookStoreApp/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils import timezone
 
 class Author(models.Model):
     # id - auto increment generated id
@@ -16,7 +17,7 @@ class Author(models.Model):
     bio = models.TextField()
     photo = models.ImageField(upload_to='author_photo/', null=True, blank=True)
     # timestamp
-    created_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True,)
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
@@ -26,8 +27,8 @@ class Category(models.Model):
     # id - auto increment generated id
     name = models.CharField(max_length=40)
     # timestamp
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True, null=True)
+    updated_at = models.DateTimeField(auto_now=True, null=True)
 
     def __str__(self):
         return self.name
@@ -35,7 +36,7 @@ class Category(models.Model):
 class Book(models.Model):
     # id - auto increment generated id
     title = models.CharField(max_length=100)
-    author = models.ForeignKey(Author, on_delete=models.DO_NOTHING)
+    author = models.ForeignKey(Author, on_delete=models.SET_NULL, null=True)
     description = models.TextField()
     price = models.DecimalField(decimal_places=2, max_digits=6)
     cover_image = models.ImageField(upload_to='book_cover/', null=True, blank=True)
@@ -43,6 +44,9 @@ class Book(models.Model):
     # timestamp
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    # fields for next migartion
+    # publised_at = models.DateTimeField(null=True)
+
 
     def __str__(self):
         return self.title
@@ -50,24 +54,24 @@ class Book(models.Model):
 class Order(models.Model):
     # id - auto increment generated id
     # data from this model is created after the call back of stripe webhook
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    book = models.ForeignKey(Book, on_delete=models.CASCADE, default=1)
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    book = models.ForeignKey(Book, on_delete=models.SET_NULL, default=1, null=True)
     # shipping_info - that will be returned from stripe
-    email = models.EmailField()
-    full_name = models.CharField()
+    email = models.EmailField(null=True)
+    full_name = models.CharField(null=True)
     country = models.CharField(max_length=50, default="Ethiopia")
-    address_line_1 = models.CharField()
-    address_line_2 = models.CharField()
-    postal_code = models.CharField()
-    city = models.CharField()
-    phone_number = models.CharField()
+    address_line_1 = models.CharField(null=True)
+    address_line_2 = models.CharField(null=True)
+    postal_code = models.CharField(null=True)
+    city = models.CharField(default="Addis Ababa", null=True)
+    phone_number = models.CharField(null=True)
     # timestamp
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
 class Payment(models.Model):
     # id - auto increment generated id
-    order = models.ForeignKey(Order, on_delete=models.CASCADE) # order id
+    order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True) # order id
     stripe_transaction_id = models.CharField(max_length=300) # stripe transaction id
     status = models.CharField(max_length=50) # pending, succeede, failed
     amount = models.DecimalField(decimal_places=2, max_digits=7) # 100
@@ -80,8 +84,8 @@ class Payment(models.Model):
 
 class Review(models.Model):
     # id - auto increment generated id
-    book = models.ForeignKey(Book, on_delete=models.CASCADE)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.SET_NULL, null=True)
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     comment = models.TextField()
     rating = models.PositiveIntegerField()
     # timestamp

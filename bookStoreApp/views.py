870887beb9562131617c3b747c7328fc1733a71e
IyasuH <eyasuhailegbr@gmail.com>
2024-04-29 11:29:17 +0300

    template and view updated, base HTML file

--- bookStoreApp/views.py
+++ bookStoreApp/views.py
@@ -7,15 +7,16 @@ from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login as login_auth, logout as logout_Auth
 
 from django.views.generic import ListView, CreateView, DetailView
+from django.db.models import Q
 
-from .models import Book, Author, Order
+from .models import Book, Author, Order, Category
 from .forms import BookCreateForm, OrderCreateForm
 
 class SignUp(CreateView):
     model=User
     form_class=UserCreationForm
     template_name='bookStoreApp/signup.html'
-    success_url='/app/login/'
+    success_url='/login/'
 
 def login(request):
     """
@@ -43,22 +44,33 @@ def logout(request):
     return redirect("list_books")
 
 # Books
-
 class ListBooks(ListView):
     model=Book
     paginate_by=10
+    def get_queryset(self):
+        search_query = self.request.GET.get('search')
+        category_query = self.request.GET.get('category')
+        queryvalue = super().get_queryset()
+        if search_query:
+            queryvalue = Book.objects.filter(Q(description__contains=search_query) | Q(title__contains=search_query))
+        elif category_query:
+            queryvalue = Book.objects.filter(categories__name__icontains=category_query)
+        return queryvalue   
+    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
+        context =  super().get_context_data(**kwargs) 
+        context['categories'] = Category.objects.all()
+        return context
 
 class CreateBook(CreateView):
     model=Book
     form_class=BookCreateForm
     template_name='bookStoreApp/_create.html'
-    success_url='/app/books/'
+    success_url='/books/'
 
 class DetailBook(DetailView):
     model=Book
 
 # Authors
-
 class ListAuthor(ListView):
     model=Author
     paginate_by=10
@@ -67,12 +79,11 @@ class DetailAuthor(DetailView):
     model=Author
 
 # Orders
-
 class CreateOrder(CreateView):
     model=Order
     form_class=OrderCreateForm
     template_name='bookStoreApp/_create.html'
-    success_url='/app/books/'
+    success_url='/books/'
 
 def addReview():
     """

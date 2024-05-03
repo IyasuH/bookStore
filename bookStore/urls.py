f08d0399fd80de3c8ccecbeb0c426cc81701dead
IyasuH <eyasuhailegbr@gmail.com>
2024-05-03 12:23:57 +0300

    requirements updated  and static file configured in urls

--- bookStore/urls.py
+++ bookStore/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 from rest_framework import routers
 from bookStoreAPI import views
@@ -31,5 +32,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("bookStoreApp.urls")),
     path("api/", include(router.urls)),
-    path("api-auth/", include('rest_framework.urls', namespace='rest_framework'))
+    path("api-auth/", include('rest_framework.urls', namespace='rest_framework')),
+    staticfiles_urlpatterns()
 ]
